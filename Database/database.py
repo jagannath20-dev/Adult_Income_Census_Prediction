807b098fbf09a403ca26fe1ef8abc6a3a086fc99
Parvez Sohail <66157611+Parvez13@users.noreply.github.com>
2021-12-03 07:54:49 +0530

    Update database.py

--- Database/database.py
+++ Database/database.py
@@ -9,27 +9,24 @@ class Connector:
         """
         self.Client_id = 'INKAkfJyEfFAyEIeORtlYYtG'
         self.Client_secret = 'FaRNXX31z4CTxb.uxO1PUkO8L4i-9wgaHyHfzbvhZ1r5yScuXJXcaihoFZyEUohHqzWRrMWvf61lPDZeznUreh221-xow1,LzLPtSuoR_cT9rxmgKns18+vGj+i,oR0,'
-        cloud_config = {
-            'secure_connect_bundle': 'secure-connect-adultincomecensusprediction.zip'}
-        auth_provider = PlainTextAuthProvider(
-            self.Client_id, self.Client_secret)
+        cloud_config = {'secure_connect_bundle': 'secure-connect-adultincomecensusprediction.zip'}
+        auth_provider = PlainTextAuthProvider(self.Client_id, self.Client_secret)
         cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
         self.session = cluster.connect()
 
     def master(self):
         """
         :DESC: Creates table if not existed into database
-        :return:
+        
         """
         self.session.execute("use adultincomecensusprediction")
         self.session.execute("select release_version from system.local")
-        self.session.execute(
-            "CREATE TABLE Data(id uuid PRIMARY KEY ,Age int,Sex text,Capital Gain int,Capital Loss int,Hours-Per-Week int,Country text,Employment Type text);")
+        self.session.execute("CREATE TABLE Data(id uuid PRIMARY KEY ,Age int,Sex text,Capital Gain int,Capital Loss int,Hours-Per-Week int,Country text,Employment Type text);")
 
     def addData(self, result):
         """
         :param result: Gets data from user and puts it into database
-        :return:
+        
         """
 
         column = "id,Age,Sex,Capital-Gain,Capital-Loss,Hours-Per-Week,Country,Employment-Type"
@@ -46,7 +43,7 @@ class Connector:
     def getData(self):
         """
         :DESC: Retrieves Data from Database
-        :return:
+        
         """
         self.session.execute("use adultincomecensus")
         row = self.session.execute("SELECT * FROM Data;")
