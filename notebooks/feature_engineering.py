ccb58e664ce33a0c09fcd393448d84e639386e7f
Parvez Sohail <66157611+Parvez13@users.noreply.github.com>
2021-12-03 08:08:04 +0530

    Update feature_engineering.py

--- notebooks/feature_engineering.py
+++ notebooks/feature_engineering.py
@@ -14,16 +14,12 @@ data = pd.read_csv('Dataset\adult.csv')
 logging.warning('-'*100)
 logging.warning('FEATURE ENGINEERING.')
 
-logging.warning(
-    "Replacing the marital-status of ['Divorced', 'Widowed','Married-spouse-absent','Never-married','Separated'] and ['Married-AF-spouse','Married-civ-spouse'] as 'Sinlge' and 'Couple' respectively. ")
+logging.warning("Replacing the marital-status of ['Divorced', 'Widowed','Married-spouse-absent','Never-married','Separated'] and ['Married-AF-spouse','Married-civ-spouse'] as 'Sinlge' and 'Couple' respectively. ")
 
-data['marital-status'] = data['marital-status'].replace(
-    ['Divorced', 'Widowed', 'Married-spouse-absent', 'Never-married', 'Separated'], 'Single', regex=True)
-data['marital-status'] = data['marital-status'].replace(
-    ['Married-AF-spouse', 'Married-civ-spouse'], 'Couple', regex=True)
+data['marital-status'] = data['marital-status'].replace(['Divorced', 'Widowed', 'Married-spouse-absent', 'Never-married', 'Separated'], 'Single', regex=True)
+data['marital-status'] = data['marital-status'].replace(['Married-AF-spouse', 'Married-civ-spouse'], 'Couple', regex=True)
 
-logging.warning(
-    "Replacing 'workclass' column with values ['Federal-gov','Local-gov','State-gov'],['Private'] ,['Self-emp-inc','Self-emp-not-inc'] and no_payment as 'govt','private' , 'self-employed' and 'without_pay' respectively.")
+logging.warning("Replacing 'workclass' column with values ['Federal-gov','Local-gov','State-gov'],['Private'] ,['Self-emp-inc','Self-emp-not-inc'] and no_payment as 'govt','private' , 'self-employed' and 'without_pay' respectively.")
 
 logging.warning('Create a function that complete the above task.')
 
@@ -39,12 +35,10 @@ def f(x):
         return 'without_pay'
 
 
-logging.warning(
-    "Create a new column 'employment-type' that contains ['govt','private','self-employed','without_pay'] values in it.")
+logging.warning("Create a new column 'employment-type' that contains ['govt','private','self-employed','without_pay'] values in it.")
 data['employment-type'] = data.apply(f, axis=1)
 
-logging.warning(
-    "Replacing values in 'country' column as 'US' for United states residency and 'Non-US' for Non-United States residency")
+logging.warning("Replacing values in 'country' column as 'US' for United states residency and 'Non-US' for Non-United States residency")
 df = [data]
 for dataset in df:
     dataset.loc[dataset['country'] != ' United-States', 'country'] = 'Non-US'
